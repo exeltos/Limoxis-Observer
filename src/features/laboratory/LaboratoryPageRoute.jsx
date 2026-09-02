246ff4d947fb28ba6bb91b0abf24d72f03db9b51
Exeltos <info@exeltos.com>
2026-09-02 16:11:27 +0300

    Remove obsolete laboratory route wrapper

--- src/features/laboratory/LaboratoryPageRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { useTenant } from '../../core/tenant/TenantContext'
-import { LaboratoryPage } from './LaboratoryPage'
-import { LaboratoryCloudPage } from './LaboratoryCloudPage'
-
-export function LaboratoryPageRoute(){
-  const {isDemo}=useTenant()
-  return isDemo?<LaboratoryPage/>:<LaboratoryCloudPage/>
-}
