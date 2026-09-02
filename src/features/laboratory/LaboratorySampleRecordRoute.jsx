68f6f8006994c581a13744dc9e2d3b442963380d
Exeltos <info@exeltos.com>
2026-09-02 16:11:37 +0300

    Remove obsolete laboratory record route wrapper

--- src/features/laboratory/LaboratorySampleRecordRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { useTenant } from '../../core/tenant/TenantContext'
-import { LaboratorySampleRecordPage } from './LaboratorySampleRecordPage'
-import { LaboratorySampleCloudRecordPage } from './LaboratorySampleCloudRecordPage'
-
-export function LaboratorySampleRecordRoute(){
-  const {isDemo}=useTenant()
-  return isDemo?<LaboratorySampleRecordPage/>:<LaboratorySampleCloudRecordPage/>
-}
