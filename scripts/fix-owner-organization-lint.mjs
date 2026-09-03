4e805d2c52617c868489ce9d9439e85aa1450100
Exeltos <info@exeltos.com>
2026-09-03 21:04:43 +0300

    chore: remove temporary owner organization lint script

--- scripts/fix-owner-organization-lint.mjs
+++
@@ -1,7 +0,0 @@
-import fs from 'node:fs'
-const path='src/features/workspaces/PlatformCenterPage.jsx'
-let s=fs.readFileSync(path,'utf8')
-s=s.replace('  Send,\n','')
-s=s.replace('  const { notify, notifyError, confirm } = useFeedback()','  const { notify, notifyError } = useFeedback()')
-s=s.replace('  function returnFromRecord() {\n    setSelectedUser(null)\n','  function returnFromRecord() {\n')
-fs.writeFileSync(path,s)
