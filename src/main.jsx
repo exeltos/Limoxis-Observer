4a5bfe4251994a3c3038813f6376056171f93be8
Exeltos <info@exeltos.com>
2026-09-05 15:09:56 +0300

    style(training): load refinement overrides

--- src/main.jsx
+++ src/main.jsx
@@ -27,6 +27,7 @@ import './styles/control-editor-fullscreen.css'
 import './styles/lira-assistant.css'
 import './styles/registry-return-highlight.css'
 import './styles/training-workspace.css'
+import './styles/training-refinement.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
