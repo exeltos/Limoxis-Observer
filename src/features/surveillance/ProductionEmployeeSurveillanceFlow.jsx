409d56947cba02c415f79b505535d4abc6375101
Exeltos <info@exeltos.com>
2026-09-13 12:37:41 +0300

    Remove obsolete repository files
    * Remove obsolete one-off workflows and legacy surveillance wrapper
    
    * Remove historical version checks and one-off maintenance scripts

--- src/features/surveillance/ProductionEmployeeSurveillanceFlow.jsx
+++
@@ -1,5 +0,0 @@
-import { EmployeeSurveillanceCanonicalFlow } from './EmployeeSurveillanceCanonicalFlow'
-
-export function ProductionEmployeeSurveillanceFlow(props){
-  return <EmployeeSurveillanceCanonicalFlow {...props}/>
-}
