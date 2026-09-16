49ff432f0af107035385160b4e130fdcb5b7d09d
Exeltos <info@exeltos.com>
2026-09-16 09:33:43 +0300

    Load clinical skeleton styles

--- src/main.jsx
+++ src/main.jsx
@@ -29,6 +29,7 @@ import './styles/prevention-refinements.css'
 import './styles/surveillance-library-dropdown-fix.css'
 import './styles/surveillance-flow-polish.css'
 import './styles/patient-workspace-polish.css'
+import './styles/clinical-loading.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
